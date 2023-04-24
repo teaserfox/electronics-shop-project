eb059fcf4fa3841259a5e2f4322e500425dfe622
Kokorina Tatyana <teaserfox@gmail.com>
2023-04-25 00:05:39 +0300

    'сделано'

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+@pytest.mark.parametrize('item1, rez', [(Item('item1', 10000, 20), 10000 * 20),
+                                        (Item('item2', 20000, 5), 20000 * 5)])
+
+def test_calculate_total_price(item1, rez):
+    assert item1.calculate_total_price() == rez
+
+
+
+
+
+
+
+
