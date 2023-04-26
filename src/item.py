10a26b283a0fb6505d94f4c4a9897e19d54c84a0
Kokorina Tatyana <teaserfox@gmail.com>
2023-04-26 13:34:00 +0300

    'ошибка открытия файла item.csv'

--- src/item.py
+++ src/item.py
@@ -1,3 +1,9 @@
+import os
+import csv
+
+PATH = os.path.csv('items.csv')
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +19,26 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """ Возвращает наименование товара
+        """
+        return f'{self.__name}'
+
+    @name.setter
+    def name(self, name) -> None:
+        """ Метод срабатывает при операции присваивания. Измеряется длинна ввода символов названия.
+        """
+        self.__name = name
+        if len(self.__name) >= 10:
+            print("Длина наименования товара превышает 10 символов.")
+        else:
+            print("Длина наименования товара меньше 10 символов")
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +54,17 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        item_list = []
+        with open('path', 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item_list.append(Item(name, price, quantity))
+            cls.all = item_list
+
+    def string_to_number(self):
+        pass
